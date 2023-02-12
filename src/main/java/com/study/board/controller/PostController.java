2fdad35ae0bcb8617721abe8e04c75cdc926901d
hunesu Kim <kewcxea@hanmail.net>
2023-02-12 22:32:36 +0900

    Merge branch 'master' of https://github.com/hunesu1114/Spring-study-forked
    # Conflicts:
    #	src/main/java/com/study/board/controller/PostController.java
    #	src/main/resources/templates/post/main.html

--- src/main/java/com/study/board/controller/PostController.java
+++ src/main/java/com/study/board/controller/PostController.java
@@ -1,7 +1,15 @@
 package com.study.board.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/post")
 public class PostController {
+
+    @GetMapping("/main")
+    public String posts() {
+        return "post/main";
+    }
 }
