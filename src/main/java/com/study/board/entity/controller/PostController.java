5aec70511ace73d47d2be9cc7d0c850cad168620
WooHCode <gusdn526@naver.com>
2023-02-13 23:48:54 +0900

    feature postController

---
+++ src/main/java/com/study/board/entity/controller/PostController.java
@@ -0,0 +1,88 @@
+package com.study.board.entity.controller;
+
+import com.study.board.entity.Post;
+import com.study.board.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+public class PostController {
+    private final PostService postService;
+
+    //생성자 주입 방식
+    //injection 방식은 총 3가지가 있다
+    //필드주입, 세터주입, 생성자주입
+    //그 중 생성자주입을 선호하는 이유는 순환참조를 방지하기 위함이다.
+    //순환참조는 컴포넌트 간 경계가 사라지고 명확한 구분점이 없어져 개발 및 유지보수에 어려움을 준다.
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "/home";
+    }
+
+
+    @GetMapping("/home")
+    public String toHome() {
+        return "/home";
+    }
+
+    @GetMapping("/posts")
+    public String posts(Model model) {
+        List<Post> allPost = postService.getAllPost();
+        model.addAttribute("form", allPost);
+        return "/postList";
+    }
+
+    @GetMapping("/posts/new")
+    public String createPost(Model model) {
+        model.addAttribute("form",new Post());
+        return "/newPost";
+    }
+
+    @PostMapping("/posts/new")
+    public String createPost(@ModelAttribute("form") Post post) {
+        postService.savePost(post);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/post/delete/{postId}")
+    public String deletePost(@PathVariable(value = "postId") Long id) {
+        postService.deletePost(id);
+        return "/postList";
+    }
+
+    @GetMapping("/post/{postId}")
+    public String post(Model model, @PathVariable(value = "postId") Long id) {
+        Post onePost = postService.getOnePost(id);
+        model.addAttribute("post", onePost);
+        return "/postDetail";
+    }
+
+    @PostMapping("/post/update/")
+    public String updatePost(@ModelAttribute Post post) {
+        postService.updatePost(post.getId(), post.getAuthor(),post.getContent(),post.getTitle());
+        return "redirect:/home";
+    }
+
+    @PostConstruct
+    public void init() {
+        for (int i = 0; i <11; i++){
+            String title = "제목" + i;
+            String author = "작성자" + i;
+            String content = "내용" + i;
+            Post post = new Post();
+            post.toEntity(author,title,content);
+            postService.savePost(post);
+        }
+    }
+}
