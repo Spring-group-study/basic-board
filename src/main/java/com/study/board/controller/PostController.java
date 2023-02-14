1db47bc18f5087f6bb1346f43db732c67548669e
black2code <alsdnr1080@gmail.com>
2023-02-14 19:35:20 +0900

    controller 구현

---
+++ src/main/java/com/study/board/controller/PostController.java
@@ -0,0 +1,53 @@
+package com.study.board.controller;
+
+import com.study.board.dto.PostDto;
+import com.study.board.entity.Post;
+import com.study.board.mapper.PostMapper;
+import com.study.board.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+
+@RestController
+@RequestMapping(path = "/v1/posts")
+public class PostController {
+
+    private final PostService postService;
+    private final PostMapper postMapper;
+
+    public PostController(PostService postService, PostMapper postMapper) {
+        this.postService = postService;
+        this.postMapper = postMapper;
+    }
+
+    @PostMapping()
+    public ResponseEntity createPost(@Valid @RequestBody PostDto.Post dto) {
+        Post post = postMapper.postDtoToPost(dto);
+        Post response = postService.createPost(post);
+        return new ResponseEntity(postMapper.postToPostResponseDto(response), HttpStatus.CREATED);
+    }
+
+    @GetMapping("{post-id}")
+    public ResponseEntity getPost(@PathVariable("post-id") @Positive long postId,
+                                   @Valid @RequestBody PostDto.Patch dto) {
+        Post response = postService.findPost(postId);
+        return new ResponseEntity(postMapper.postToPostResponseDto(response), HttpStatus.OK);
+    }
+
+    @PatchMapping("{post-id}")
+    public ResponseEntity patchPost(@PathVariable("post-id") @Positive long postId,
+                                    @Valid @RequestBody PostDto.Patch dto) {
+        Post post = postMapper.postPatchDtoToPost(dto);
+        Post response = postService.updatePost(post, postId);
+        return new ResponseEntity(postMapper.postToPostResponseDto(response), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{post-id}")
+    public ResponseEntity deletePost(@PathVariable("post-id") @Positive long postId) {
+        postService.deletePost(postId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
