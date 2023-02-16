a1af8b5f9364a003921285f13031b1febff93f10
WooHCode <gusdn526@naver.com>
2023-02-17 01:56:32 +0900

    update v2( repository 구현완료)

--- src/main/java/com/study/board/repository/PostRepositoryV2Impl.java
+++ src/main/java/com/study/board/repository/PostRepositoryV2Impl.java
@@ -3,11 +3,16 @@ package com.study.board.repository;
 import com.study.board.entity.Post;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Repository
 public class PostRepositoryV2Impl implements PostRepositoryV2{
@@ -35,17 +40,28 @@ public class PostRepositoryV2Impl implements PostRepositoryV2{
 
     @Override
     public void save(Post post) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("post").usingGeneratedKeyColumns("id");
 
+        Map<String, String> data = new HashMap<>();
+        data.put("content", post.getContent());
+        data.put("author", post.getAuthor());
+        data.put("title", post.getTitle());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(data));
+        post.setId(key.longValue());
     }
 
     @Override
     public void update(Long id, String author, String content, String title) {
-
+        String sql = "update post set author=?, content=?, title=? where id=?";
+        jdbcTemplate.update(sql,content,title,author,id);
     }
 
     @Override
     public void delete(Long id) {
-
+        String sql = "delete from post where id=?";
+        jdbcTemplate.update(sql,id);
     }
 
     static class PostMapper implements RowMapper<Post> {
