aa38dd12402912f42530c0386521274a4ef984e0
WooHCode <gusdn526@naver.com>
2023-03-17 11:06:25 +0900

    세션로그인( id 와 pw 조회 후 계정이있으면 로그인)

--- src/main/java/com/study/board/controller/HomeController.java
+++ src/main/java/com/study/board/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Member;
+import com.study.board.resolver.Login;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String homePage(@Login Member loginMember) {
+        if (loginMember == null) {
+            return "loginForm";
+        }
+        return "home";
+    }
+
     @GetMapping("/home")
-    public String toHome() {
+    public String goToHome() {
         return "home";
     }
 }
